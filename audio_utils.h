6395b69cba8f8bd05566889128d6e10f5e043747
Vasanthi B N S <127195382+VB-123@users.noreply.github.com>
2024-10-13 20:08:05 +0530

    Update audio_utils.h

--- audio_utils.h
+++ audio_utils.h
@@ -1,17 +1,13 @@
-/*
- * audio_utils.h
- *
- *  Created on: 13-Oct-2024
- *      Author: 91884
- */
-
 #ifndef AUDIO_UTILS_H_
 #define AUDIO_UTILS_H_
 
 #include <stdint.h>
 #include <stddef.h>
 
-void Wav_To_Num(const char *filename, int16_t **samples, size_t *num_samples);
-void Num_to_wav(const char *filename, int16_t *samples, size_t num_samples);
+void Wav_To_Num(const char *filename, int16_t **samples, unsigned int *num_samples);
+void Num_to_wav(const char *filename, int16_t *samples, unsigned int num_samples);
+void scale_audio(int16_t *samples, unsigned int num_samples, float scaling_factor);
+void reverse_audio(int16_t *samples,  unsigned int num_samples);
+void swap_chunks(int16_t *samples, unsigned int num_samples);
 
 #endif /* AUDIO_UTILS_H_ */
